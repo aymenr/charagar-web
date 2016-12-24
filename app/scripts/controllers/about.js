21cb16b94dfb29460ddd11af625b54919485d3d4
aymenr <aymenrizwan93@gmail.com>
2016-12-25 01:05:21 +0500

    angular project init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name charagarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the charagarApp
+ */
+angular.module('charagarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name charagarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the charagarApp
+ */
+angular.module('charagarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
