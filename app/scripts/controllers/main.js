06ce58c2143f2e996977868584b9122df3e78244
aymenr <aymenrizwan93@gmail.com>
2016-12-25 01:39:59 +0500

    modularized header

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the charagarApp
  */
 angular.module('charagarApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+  	function init()
+  	{
+  		$scope.isLoggedIn = false;
+  	}
+
+
+
+  	    $scope.openSignup = function()
+    {
+    	console.log("SHOULD POPUP");
+        // ngDialog.open(
+        // {
+        //     template: 'views/user/userAuth.html',
+        //     controller: 'UserauthCtrl',
+        //     className: 'ngdialog-theme-default userAuth',
+        //     showClose: true,
+        //     closeByEscape: true,
+        //     closeByDocument: true
+
+        // });
+    }
+
+
+
   });
