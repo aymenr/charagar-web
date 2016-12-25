7dd618d92bf0795f113f05e311da1261b430cf46
aymenr <aymenrizwan93@gmail.com>
2016-12-25 23:02:23 +0500

    fix routing problems

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,26 +8,37 @@
  *
  * Main module of the application.
  */
-angular
-  .module('charagarApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ui.router',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ngDialog'
+ angular
+ .module('charagarApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ngDialog'
 
   ])
-  .config(function ($stateProvider) {
+  .config(function ($routeProvider,$locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+,        controllerAs: 'about'
+      })
+       .when('/startCampaign', {
+        templateUrl: 'views/startCampaign.html',
+        controller: 'StartcampaignCtrl',
+        controllerAs: 'startCampaign'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
 
-    $stateProvider
-      .state('about',
-        {
-            url: "/about",
-            templateUrl: "views/about.html",
-            controller: "AboutCtrl"
-        })
-
+      $locationProvider.html5Mode(true);
   });
