75db1a7133ee7dad5d124a2ee339ab5d11665c8d
aymenr <aymenrizwan93@gmail.com>
2016-12-25 18:59:16 +0500

    sign up implemented

--- app/scripts/controllers/userauth.js
+++ app/scripts/controllers/userauth.js
@@ -10,26 +10,56 @@
  angular.module('charagarApp')
  .controller('UserauthCtrl', function ($scope,userService,ngDialog) {
 
- 		$scope.isError = false;
-
-        $scope.loginUser = function()
-        {
-        	console.log("LOGIN USER,", $scope.user);
-            $scope.isError = false;
-            userService.loginUser(angular.copy($scope.user)).then(function(data)
-                {
+   $scope.isError = false;
+   $scope.state = "login";
+   $scope.loginUser = function()
+   {
+     console.log("LOGIN USER,", $scope.user);
+     $scope.isError = false;
+     userService.loginUser(angular.copy($scope.user)).then(function(data)
+     {
                 	//$.go("/#!");
-                     $scope.closeThisDialog();
-                	console.log("HO GAYA LOGIN WOW");
+                 $scope.closeThisDialog();
+                 console.log("HO GAYA LOGIN WOW");
                     //$scope.go("/home/dashboard");
                 },
                 function(errorMessage)
                 {
                     $scope.isError = true;
                 });
-        }
+ }
 
 
+ $scope.switchView = function(isSignup)
+ {
+    if(isSignup) {
+        console.log("switch to signup");
+        $scope.state ="signup";
+    } else {
+        $scope.state="login";
+    }
+}
+
+$scope.signupUser = function(type)
+{
+    console.log("Going to sign up");
+    $scope.isError = false;
+    var udata = angular.copy($scope.user);
+    console.log("copied u data:", udata);
+
+    userService.signupUser(angular.copy($scope.user)).then(function(data)
+    {
+        console.log("SIGNUPED");
+        $scope.closeThisDialog();
+        console.log("HO GAYA signup");
+                    //$scope.go("/home/dashboard");
+                },
+                function(errorMessage)
+                {
+                    $scope.isError = true;
+                });
 
 
- });
+};
+
+})
--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -8,14 +8,15 @@
  * Service in the charagarApp.
  */
 angular.module('charagarApp')
-  .service('userService', function (configConstants, $cookieStore, $http) {
-
-
-        var apiPrefix = configConstants.apiPrefix;
+  .service('userService', function (configConstants, $cookieStore, $http,$rootScope) {
         var userInfo;
+  var apiPrefix = configConstants.apiPrefix;
 
         function init()
         {
+
+
+
             var cookieInfo = $cookieStore.get('userInfo')
             if (cookieInfo)
             {
@@ -54,6 +55,41 @@ angular.module('charagarApp')
             userInfo = null;
             $cookieStore.remove('userInfo')
         }
+
+        this.signupUser = function(userData)
+        {
+            console.log("GONNA SIGN UP IN SERVICE WOW");
+
+            return $http.post(apiPrefix + 'signupUser', angular.toJson(userData, true)).then(function(result)
+            {
+                if (!result.data.error)
+                {   console.log("signup, gonna broadcast login:", result.data);
+                    saveUserInfo(result.data);
+
+                    $rootScope.$broadcast("userLogin");
+                    return result.data;
+                }
+                else
+                {
+                    //error here
+                    console.log("signup failed");
+                }
+
+
+            });
+        }
+
+        function saveUserInfo(info)
+        {
+            console.log("IN SAVE USER INFO:", info);
+            userInfo = info;
+
+            userInfo.isLoggedIn = true;
+           $cookieStore.put('userInfo', userInfo);
+                console.log("RESSSS:", $cookieStore.get('userInfo'));
+            return
+        }
+
 
 
   });
