da45d336ca196db496e69f1b4aee68adc074577b
aymenr <aymenrizwan93@gmail.com>
2017-02-03 20:03:30 +0500

    api url changed to heroku

--- app/scripts/services/configconstants.js
+++ app/scripts/services/configconstants.js
@@ -10,10 +10,10 @@
  angular.module('charagarApp')
  .constant('configConstants',
  {
- 	apiPrefix: "http://localhost:3000/",
- 	authorizedApiPrefix: "http://localhost:3000/api/v1/",
- 	adminApiPrefix: "http://localhost:3000/api/v1/admin/",
- 	userApiPrefix:"http://localhost:3000/api/v1/user/",
+ 	apiPrefix: "https://charagar.herokuapp.com/",
+ 	authorizedApiPrefix: "https://charagar.herokuapp.com/api/v1/",
+ 	adminApiPrefix: "https://charagar.herokuapp.com/api/v1/admin/",
+ 	userApiPrefix:"https://charagar.herokuapp.com/api/v1/user/",
  	imgurClientID:"4c8a0a606234adf",
  	imgurClientSecret:"2b63ec31c9df5fbcd561e4e81b68b0c1db8b4889"
  });
